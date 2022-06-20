2e4c9affbb0a1b44e3564604fa1223a26c130c54
SLNY1921394421 <1921394421@qq.com>
2022-06-20 17:19:34 +0800

    【reactive】----isReactive及isReadOnly

--- src/reactivity/__test__/readonly.spec.ts
+++ src/reactivity/__test__/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { readonly } from "../reactive";
+import { isReadonly, readonly } from "../reactive";
 
 describe("readonly", () => {
 	it("happy path", () => {
@@ -6,6 +6,8 @@ describe("readonly", () => {
 		const warped = readonly(original);
 		expect(warped).not.toBe(original);
 		expect(warped.foo).toBe(1);
+		expect(isReadonly(warped)).toBe(true);
+		expect(isReadonly(original)).toBe(false);
 	});
 	it("warn when call set", () => {
 		console.warn = jest.fn();
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,6 +1,13 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 // import { track, trigger } from "./effect";
 
+
+export const enum ReactiveFlags {
+	IS_REACTIVE = '__v-isReactive',
+	IS_READONLY = '__v_isReadonly',
+	RAW = '__v-raw',
+}
+
 export function reactive(raw) {
 	// return new Proxy(raw, mutableHandlers)
 
@@ -46,4 +53,13 @@ export function readonly(raw) {
 
 function createActiveObject(raw, baseHandlers) {
 	return new Proxy(raw, baseHandlers);
+}
+
+export function isReactive(value) {
+	return !!value[ReactiveFlags.IS_REACTIVE]
+}
+
+
+export function isReadonly(value) {
+	return !!value[ReactiveFlags.IS_READONLY]
 }
