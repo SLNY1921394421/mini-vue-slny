846b30b41b89b9e7b244266d40fe00bed509bb45
SLNY1921394421 <1921394421@qq.com>
2022-06-28 14:42:54 +0800

    实现 shapeFlags

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared";
+import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 import { createVnode } from "./vnode";
 
@@ -12,9 +13,10 @@ export function render(vnode, container) {
  * @param container
  */
 function patch(vnode, container) {
-	if(typeof vnode.type === 'string') {
+	const { shapeFlag } = vnode
+	if(shapeFlag & ShapeFlags.ELEMENT) {
 		processElement(vnode, container)
-	} else if(isObject(vnode.type)) {
+	} else if(shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
 		processComponent(vnode, container)
 	}
 }
@@ -43,10 +45,10 @@ function processElement(vnode: any, container: any) {
 
 function mountElement(vnode: any, container: any) {
 	const el = (vnode.el =  document.createElement(vnode.type));
-	const { children, props } = vnode;
-	if(typeof children === 'string') {
+	const { children, props, shapeFlag } = vnode;
+	if(shapeFlag & ShapeFlags.TEXT_CHILDREN) {
 		el.textContent = children;
-	} else if(Array.isArray(children)) {
+	} else if(shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
 		// children.forEach(v => {
 		// 	patch(v, el);
 		// })
